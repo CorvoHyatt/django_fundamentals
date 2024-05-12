51ff3b2d312df51c72d183fe1dcbf7d8805a0597
CorvoHyatt <corvohyatt@gmail.com>
2024-05-12 13:42:39 -0600

    Templates updated

--- myapp/views.py
+++ myapp/views.py
@@ -5,9 +5,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    title = "Welcome to django course"
     return render(
         request,
         "index.html",
+        {
+            "title": title,
+        },
     )
 
 
