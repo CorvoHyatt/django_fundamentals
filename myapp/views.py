f5f3426a20b27b6adb6055d3fc3112d59ea447aa
CorvoHyatt <corvohyatt@gmail.com>
2024-05-12 11:02:12 -0600

    Jsonresponse added

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Proyect, Task
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>hello Laura<h1>")
+def index(request):
+    return HttpResponse("index page")
+
+
+def hello(request, username: str):
+    return HttpResponse(f"<h1>hello {username}<h1>")
 
 
 def about(request):
     return HttpResponse("about")
+
+
+def projects(request):
+    projects = list(Proyect.objects.values())
+    return JsonResponse(projects, safe=False)
+
+
+def tasks(request, id: int):
+    query = Task.objects
+    # task = query.get(id=id)
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse(f"task: {task.title}")
