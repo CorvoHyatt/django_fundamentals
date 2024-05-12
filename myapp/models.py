c9e25bb20561d0c79a67d94c1bfe2b2c88c6410e
CorvoHyatt <corvohyatt@gmail.com>
2024-05-12 17:09:46 -0600

    Conditional jinja stuff update

--- myapp/models.py
+++ myapp/models.py
@@ -13,6 +13,7 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     descritption = models.TextField()
     project = models.ForeignKey(Proyect, on_delete=models.CASCADE)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.title} - {self.project.name}"
